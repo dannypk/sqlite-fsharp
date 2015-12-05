057d00fcd70947f586b311b85177880562dbb638
dannypk <pacurici_daniel2004@yahoo.com>
2015-12-04 17:26:35 -0800

    sqlite first attempt

---
+++ sqlite/Database.fs
@@ -0,0 +1,58 @@
+﻿namespace SQLite
+
+open System.Data.SQLite
+
+module Database =
+
+    type Result = Success | Failure  
+    
+    let databaseName = "..\hr-cooper.logging.sqlite"
+    let connectionString = sprintf "Data Source=%s;Version=3" databaseName
+
+    let tableStructure = @"CREATE TABLE IF NOT EXISTS DEPLOYMENT (
+                            _id INTEGER PRIMARY KEY AUTOINCREMENT,
+                            IpAddress TEXT,
+                            DeploymentTime TEXT,
+                            Environment TEXT,
+                            Service TEXT,
+                            DeploymentResult TEXT)"
+                          
+    let insertQuery IP deploymentTime environment service deploymentResult = 
+        sprintf @"INSERT INTO DEPLOYMENT (IpAddress, DeploymentTime, Environment, Service, DeploymentResult)
+                    values ('%s', '%s', '%s', '%s', '%s')" IP deploymentTime environment service deploymentResult
+
+    let private createTable() = 
+        try
+            let cn = new SQLiteConnection(connectionString)
+            cn.Open()
+
+            let createTableScript = tableStructure
+            let cmd = new SQLiteCommand(createTableScript, cn)
+            let result = cmd.ExecuteNonQuery()
+            cn.Close()
+            
+            Success
+        with
+            | _ -> Failure
+
+
+    let insert IP deploymentTime environment service deploymentResult = 
+        if createTable() = Success 
+            then
+                try
+                    let cn = new SQLiteConnection(connectionString)
+                    cn.Open()
+
+                    let sql = insertQuery IP deploymentTime environment service deploymentResult
+                    let cmd = new SQLiteCommand(sql, cn)
+                    let result = cmd.ExecuteNonQuery()
+        
+                    cn.Close()
+                    
+                    if result = -1 then Failure
+                    else Success
+                with 
+                    | _ -> Failure
+
+            else Failure 
+                
---
+++ sqlite/Program.fs
@@ -0,0 +1,15 @@
+﻿// Learn more about F# at http://fsharp.net
+// See the 'F# Tutorial' project for more help.
+
+open SQLite.Database
+open System
+
+[<EntryPoint>]
+let main argv = 
+    //createDatabase
+    let result = insert "192.168.0.123" "2015-12-04" "int" "contract" "success"
+    //IP deploymentTime environment service deploymentResult
+    Console.WriteLine(result)
+    
+    //Console.ReadKey() |> ignore;
+    0 // return an integer exit code
